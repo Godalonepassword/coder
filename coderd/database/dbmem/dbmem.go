581aafc100418d08003ac5a0cc81acc3c7e348bd
Danny Kopping <danny@coder.com>
2024-11-21 16:29:40 +0000

    Working implementation
    Signed-off-by: Danny Kopping <danny@coder.com>

--- coderd/database/dbmem/dbmem.go
+++ coderd/database/dbmem/dbmem.go
@@ -8173,6 +8173,7 @@ func (q *FakeQuerier) InsertWorkspaceApp(_ context.Context, arg database.InsertW
 		Health:               arg.Health,
 		Hidden:               arg.Hidden,
 		DisplayOrder:         arg.DisplayOrder,
+		CorsBehavior:         arg.CorsBehavior,
 	}
 	q.workspaceApps = append(q.workspaceApps, workspaceApp)
 	return workspaceApp, nil
